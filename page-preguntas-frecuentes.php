c5c17f4010f96c197070a13d374346d6c8f527a9
Roger <roger.masellas@inthe.es>
2024-07-03 20:22:47 +0200

    faq

--- page-preguntas-frecuentes.php
+++ page-preguntas-frecuentes.php
@@ -29,62 +29,67 @@ get_header();
                 </div>
                 <div class="col-12">
                     <h2 class="title">
-                        Ayuda
+                        Preguntes Freqüents
                     </h2>
                 </div>
             </div>
         </div>
     </section>
+    
     <section class="PreguntasFrecuentes">
-        <div class="PreguntasFrecuentes-container">
-            <h3 class=PreguntasFrecuentes-title>
-                Preguntas frecuentes
-            </h3>
-            <div class="PreguntasFrecuentes-modal" id="questionTextBox">
-            <div class="PreguntasFrecuentes-close" id="questionCloseBtn">
-            <img class="img-fluid PreguntasFrecuentes-icon " src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-closeIcon.png">
-            </div>
-                <h5 class="">
-                1€ = 1 Vale.
-                </h5>
-                <p>
-                En esta plataforma,1 euro equivale a 1 Vale. En la sección "Mi Saldo," tendrás una visualización clara de la cantidad de vales disponibles.
-                </p>
-            </div>
-            <div class="PreguntasFrecuentes-content">
-                <button class="PreguntasFrecuentes-btn">
-                <img class="img-fluid PreguntasFrecuentes-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-icon1-cards-help.png" alt="valeapp">
-                Mi cuenta
-                </button>
-                <button class="PreguntasFrecuentes-btn">
-                <div class="PreguntasFrecuentes-boxImg">
-                    <img class="img-fluid PreguntasFrecuentes-icon icon2" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeap-arrow-promociones.png" alt="valeapp">
-                    <span class="PreguntasFrecuentes-question">
-                        ?
-                    </span>
+        <div class="container">
+            <div class="row">
+                <div class="PreguntasFrecuentes-container">
+                    <div class="PreguntasFrecuentes-modal" id="questionTextBox">
+                        <div class="PreguntasFrecuentes-close" id="questionCloseBtn">
+                        <img class="img-fluid PreguntasFrecuentes-icon " src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-closeIcon.png">
+                    </div>
+                    <h5 class="">
+                    1€ = 1 Vale.
+                    </h5>
+                    <p>
+                    En esta plataforma,1 euro equivale a 1 Vale. En la sección "Mi Saldo," tendrás una visualización clara de la cantidad de vales disponibles.
+                    </p>
                 </div>
-                Mis Vales
-                </button>
-                <button class="PreguntasFrecuentes-btn">
-                <img class="img-fluid PreguntasFrecuentes-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-icon3-cards-help.png" alt="valeapp">
-                Mis reservas
-                </button>
-                <button class="PreguntasFrecuentes-btn">
-                <img class="img-fluid PreguntasFrecuentes-icon icon2" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-icon4-cards-help.png" alt="valeapp">
-                Importante
-                </button>
-                <button class="PreguntasFrecuentes-btn">
-                <img class="img-fluid PreguntasFrecuentes-icon icon2" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-icon5-cards-help.png" alt="valeapp">
-                Qué pasa si?
-                </button>
             </div>
-            <div class="Ayuda-footer">
-                <a class="Ayuda-help" href="/ayuda">
-                    ¿Necesitas ayuda?
-                </a>
-                <a class="Ayuda-delete" href="/eliminar-cuenta">
-                    Eliminar cuenta
-                </a>
+            <div class="row">
+                <div class="PreguntasFrecuentes-content">
+                    <button class="PreguntasFrecuentes-btn">
+                    <img class="img-fluid PreguntasFrecuentes-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-icon1-cards-help.png" alt="valeapp">
+                    Mi cuenta
+                    </button>
+                    <button class="PreguntasFrecuentes-btn">
+                    <div class="PreguntasFrecuentes-boxImg">
+                        <img class="img-fluid PreguntasFrecuentes-icon icon2" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeap-arrow-promociones.png" alt="valeapp">
+                        <span class="PreguntasFrecuentes-question">
+                            ?
+                        </span>
+                    </div>
+                    Mis Vales
+                    </button>
+                    <button class="PreguntasFrecuentes-btn">
+                    <img class="img-fluid PreguntasFrecuentes-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-icon3-cards-help.png" alt="valeapp">
+                    Mis reservas
+                    </button>
+                    <button class="PreguntasFrecuentes-btn">
+                    <img class="img-fluid PreguntasFrecuentes-icon icon2" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-icon4-cards-help.png" alt="valeapp">
+                    Importante
+                    </button>
+                    <button class="PreguntasFrecuentes-btn">
+                    <img class="img-fluid PreguntasFrecuentes-icon icon2" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-icon5-cards-help.png" alt="valeapp">
+                    Qué pasa si?
+                    </button>
+                </div>
+            </div>
+            <div class="row">                
+                <div class="Ayuda-footer">
+                    <a class="Ayuda-help" href="/ayuda">
+                        ¿Necesitas ayuda?
+                    </a>
+                    <a class="Ayuda-delete" href="/eliminar-cuenta">
+                        Eliminar cuenta
+                    </a>
+                </div>
             </div>
         </div>
     </section>
