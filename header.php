d1a6371bd839da4bbc4dd471473d1c846dca9fa0
Roger <roger.masellas@inthe.es>
2024-07-04 11:43:56 +0200

    header cleaning

--- header.php
+++ header.php
@@ -69,7 +69,6 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										Llar
 									</button>
 									<div class="collapse" id="collapsedService1">
-<<<<<<< HEAD
 										<a href="/categories/lampista" class="NavMenu-service-link">Lampista</a>
 										<a href="/categories/neteja" class="NavMenu-service-link">Neteja</a>
 										<a href="/categories/pintar_cases" class="NavMenu-service-link">Pintura</a>
@@ -79,17 +78,6 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										<a href="/categories/muntatge_mobles" class="NavMenu-service-link">Muntatge mobles</a>
 										<a href="/categories/mecanic" class="NavMenu-service-link">Mecànic</a>
 										<a href="/categories/solucions_eco" class="NavMenu-service-link">Solucions ECO</a>
-=======
-										<a href="/categories/fontaneria" class="NavMenu-service-link">Lampista</a>
-										<a href="/categories/limpieza" class="NavMenu-service-link">Neteja</a>
-										<a href="/categories/pintura" class="NavMenu-service-link">Pintura</a>
-										<a href="/categories/mudanza" class="NavMenu-service-link">Mudança</a>
-										<a href="/categories/obras" class="NavMenu-service-link">Obres</a>
-										<a href="/categories/electricidad" class="NavMenu-service-link">Electricitat</a>
-										<a href="/categories/montaje-muebles" class="NavMenu-service-link">Muntatge mobles</a>
-										<a href="/categories/mecanico" class="NavMenu-service-link">Mecànic</a>
-										<a href="/categories/soluciones-eco" class="NavMenu-service-link">Solucions ECO</a>
->>>>>>> f4a7216dd0602431414c33c60f836de7e29fa3ff
 									</div>
 								</div>
 								<div class="NavMenu-service">
@@ -98,7 +86,6 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										Aprèn
 									</button>
 									<div class="collapse" id="collapsedService2">
-<<<<<<< HEAD
 										<a href="/categories/idiomes" class="NavMenu-service-link">Idiomes</a>
 										<a href="/categories/instruments" class="NavMenu-service-link">Instruments</a>
 										<a href="/categories/classes_de_repas" class="NavMenu-service-link">Clases de repàs</a>
@@ -109,18 +96,6 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										<a href="/categories/jocs" class="NavMenu-service-link">Jocs</a>
 										<a href="/categories/poquer" class="NavMenu-service-link">Pòquer</a>
 										<a href="/categories/cuina" class="NavMenu-service-link">Cuina</a>
-=======
-										<a href="/categories/idiomas" class="NavMenu-service-link">Idiomes</a>
-										<a href="/categories/instrumentos" class="NavMenu-service-link">Instruments</a>
-										<a href="/categories/clases-de-repaso" class="NavMenu-service-link">Clases de repàs</a>
-										<a href="/categories/baile" class="NavMenu-service-link">Ball</a>
-										<a href="/categories/inversiones" class="NavMenu-service-link">Inversions</a>
-										<a href="/categories/pintura" class="NavMenu-service-link">Pintura</a>
-										<a href="/categories/fotografia" class="NavMenu-service-link">Fotografia</a>
-										<a href="/categories/gaming" class="NavMenu-service-link">Jocs</a>
-										<a href="/categories/poker" class="NavMenu-service-link">Pòquer</a>
-										<a href="/categories/cocina" class="NavMenu-service-link">Cuina</a>
->>>>>>> f4a7216dd0602431414c33c60f836de7e29fa3ff
 									</div>
 								</div>
 								<div class="NavMenu-service">
@@ -129,7 +104,6 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										Esport
 									</button>
 									<div class="collapse" id="collapsedService3">
-<<<<<<< HEAD
 										<a href="/categories/gimnas" class="NavMenu-service-link">Gimnàs</a>
 										<a href="/categories/arts_marcials" class="NavMenu-service-link">Arts marcials</a>
 										<a href="/categories/amb_pilota" class="NavMenu-service-link">Amb pilota</a>
@@ -137,15 +111,6 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										<a href="/categories/al_aigua" class="NavMenu-service-link">A l'aigua</a>
 										<a href="/categories/al_aire" class="NavMenu-service-link">A l'aire</a>
 										<a href="/categories/sobre_rodes" class="NavMenu-service-link">Sobre rodes</a>
-=======
-										<a href="/categories/gim" class="NavMenu-service-link">Gimnàs</a>
-										<a href="/categories/artes-marciales" class="NavMenu-service-link">Arts marcials</a>
-										<a href="/categories/con-pelota" class="NavMenu-service-link">Amb pilota</a>
-										<a href="/categories/con-raqueta" class="NavMenu-service-link">Amb raqueta</a>
-										<a href="/categories/al-agua" class="NavMenu-service-link">A l'aigua</a>
-										<a href="/categories/al-aire" class="NavMenu-service-link">A l'aire</a>
-										<a href="/categories/sobre-ruedas" class="NavMenu-service-link">Sobre rodes</a>
->>>>>>> f4a7216dd0602431414c33c60f836de7e29fa3ff
 									</div>
 								</div>
 								<div class="NavMenu-service">
@@ -154,7 +119,6 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										Ment
 									</button>
 									<div class="collapse" id="collapsedService4">
-<<<<<<< HEAD
 										<a href="/categories/entrenador" class="NavMenu-service-link">Coach</a>
 										<a href="/categories/psicologia" class="NavMenu-service-link">Psicologia</a>
 										<a href="/categories/tarot" class="NavMenu-service-link">Tarot</a>
@@ -162,15 +126,6 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										<a href="/categories/meditacio" class="NavMenu-service-link">Meditació</a>
 										<a href="/categories/ioga" class="NavMenu-service-link">Ioga</a>
 										<a href="/categories/terapies_alternatives" class="NavMenu-service-link">Teràpies alternatives</a>
-=======
-										<a href="/categories/coach" class="NavMenu-service-link">Coach</a>
-										<a href="/categories/psicologia" class="NavMenu-service-link">Psicologia</a>
-										<a href="/categories/tarot" class="NavMenu-service-link">Tarot</a>
-										<a href="/categories/terapias-de-pareja" class="NavMenu-service-link">Teràpies de parella</a>
-										<a href="/categories/meditacion" class="NavMenu-service-link">Meditació</a>
-										<a href="/categories/yoga" class="NavMenu-service-link">Ioga</a>
-										<a href="/categories/terapias-alternativas" class="NavMenu-service-link">Teràpies alternatives</a>
->>>>>>> f4a7216dd0602431414c33c60f836de7e29fa3ff
 									</div>
 								</div>
 								<div class="NavMenu-service">
@@ -179,7 +134,6 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										Cures
 									</button>
 									<div class="collapse" id="collapsedService5">
-<<<<<<< HEAD
 										<a href="/categories/mascotes" class="NavMenu-service-link">Mascotes</a>
 										<a href="/categories/nens" class="NavMenu-service-link">Nens</a>
 										<a href="/categories/gent_gran" class="NavMenu-service-link">Gent gran</a>
@@ -187,15 +141,6 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										<a href="/categories/seguretat" class="NavMenu-service-link">Seguretat</a>
 										<a href="/categories/massatges" class="NavMenu-service-link">Massatges</a>
 										<a href="/categories/dietes" class="NavMenu-service-link">Dietes</a>
-=======
-										<a href="/categories/mascotas" class="NavMenu-service-link">Mascotes</a>
-										<a href="/categories/ninos" class="NavMenu-service-link">Nens</a>
-										<a href="/categories/personas-mayores" class="NavMenu-service-link">Gent gran</a>
-										<a href="/categories/au-pair" class="NavMenu-service-link">Au pair</a>
-										<a href="/categories/seguridad" class="NavMenu-service-link">Seguretat</a>
-										<a href="/categories/masajes" class="NavMenu-service-link">Massatges</a>
-										<a href="/categories/dietas" class="NavMenu-service-link">Dietes</a>
->>>>>>> f4a7216dd0602431414c33c60f836de7e29fa3ff
 									</div>
 								</div>
 								<div class="NavMenu-service">
@@ -204,19 +149,11 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										Bellesa
 									</button>
 									<div class="collapse" id="collapsedService6">
-<<<<<<< HEAD
 										<a href="/categories/perruqueria" class="NavMenu-service-link">Perruqueria</a>
 										<a href="/categories/tractaments" class="NavMenu-service-link">Tractaments</a>
 										<a href="/categories/ungles" class="NavMenu-service-link">Ungles</a>
 										<a href="/categories/maquillatge" class="NavMenu-service-link">Maquillatge</a>
 										<a href="/categories/depilacio" class="NavMenu-service-link">Depilació</a>
-=======
-										<a href="/categories/peluqueria" class="NavMenu-service-link">Perruqueria</a>
-										<a href="/categories/tratamientos" class="NavMenu-service-link">Tractaments</a>
-										<a href="/categories/unas" class="NavMenu-service-link">Ungles</a>
-										<a href="/categories/maquillaje" class="NavMenu-service-link">Maquillatge</a>
-										<a href="/categories/depilacion" class="NavMenu-service-link">Depilació</a>
->>>>>>> f4a7216dd0602431414c33c60f836de7e29fa3ff
 										<a href="/categories/dental" class="NavMenu-service-link">Dental</a>
 									</div>
 								</div>
@@ -226,7 +163,6 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										Contracta
 									</button>
 									<div class="collapse" id="collapsedService7">
-<<<<<<< HEAD
 										<a href="/categories/publicitat" class="NavMenu-service-link">Publicitat</a>
 										<a href="/categories/desenvolupament_i_app" class="NavMenu-service-link">Desenvolupament i app</a>
 										<a href="/categories/analisi_de_dades" class="NavMenu-service-link">Anàlisi de dades</a>
@@ -237,18 +173,6 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										<a href="/categories/hostaleria" class="NavMenu-service-link">Hostaleria</a>
 										<a href="/categories/guia_turistic" class="NavMenu-service-link">Guia turistic</a>
 										<a href="/categories/agent_viatges" class="NavMenu-service-link">Agent viatges</a>
-=======
-										<a href="/categories/publicidad" class="NavMenu-service-link">Publicitat</a>
-										<a href="/categories/desarrollo-y-app" class="NavMenu-service-link">Desenvolupament i app</a>
-										<a href="/categories/analisis-de-datos" class="NavMenu-service-link">Anàlisi de dades</a>
-										<a href="/categories/legal" class="NavMenu-service-link">Legal</a>
-										<a href="/categories/traducciones" class="NavMenu-service-link">Traduccions</a>
-										<a href="/categories/redes-sociales" class="NavMenu-service-link">Xarxes socials</a>
-										<a href="/categories/foto-video" class="NavMenu-service-link">Foto / Video</a>
-										<a href="/categories/hosteleria" class="NavMenu-service-link">Hostaleria</a>
-										<a href="/categories/guia-turistico" class="NavMenu-service-link">Guia turistic</a>
-										<a href="/categories/agente-viajes" class="NavMenu-service-link">Agent viatges</a>
->>>>>>> f4a7216dd0602431414c33c60f836de7e29fa3ff
 										<a href="/categories/influencers" class="NavMenu-service-link">Influencers</a>
 									</div>
 								</div>
@@ -258,17 +182,10 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 										Comparteix
 									</button>
 									<div class="collapse" id="collapsedService8">
-<<<<<<< HEAD
 										<a href="/categories/espai" class="NavMenu-service-link">Espai</a>
 										<a href="/categories/estudi" class="NavMenu-service-link">Estudi</a>
 										<a href="/categories/consulta" class="NavMenu-service-link">Consulta</a>
 										<a href="/categories/equips" class="NavMenu-service-link">Equips</a>
-=======
-										<a href="/categories/espacio" class="NavMenu-service-link">Espai</a>
-										<a href="/categories/estudio" class="NavMenu-service-link">Estudi</a>
-										<a href="/categories/consulta" class="NavMenu-service-link">Consulta</a>
-										<a href="/categories/equipos" class="NavMenu-service-link">Equips</a>
->>>>>>> f4a7216dd0602431414c33c60f836de7e29fa3ff
 										<a href="/categories/material" class="NavMenu-service-link">Material</a>
 										<a href="/categories/pista" class="NavMenu-service-link">Pista</a>
 									</div>
@@ -281,11 +198,7 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 									<div class="collapse" id="collapsedService9">
 										<a href="/categories/empreses" class="NavMenu-service-link">Empreses</a>
 										<a href="/categories/noces" class="NavMenu-service-link">Noces</a>
-<<<<<<< HEAD
 										<a href="/categories/comiats_o_anniversari" class="NavMenu-service-link">Comiats / anniversari</a>
-=======
-										<a href="/categories/comiats_o_aniversari" class="NavMenu-service-link">Comiats / anniversari</a>
->>>>>>> f4a7216dd0602431414c33c60f836de7e29fa3ff
 										<a href="/categories/dj_o_musica_en_viu" class="NavMenu-service-link">DJ / Musica en viu</a>
 										<a href="/categories/promocionals" class="NavMenu-service-link">Promocionals</a>
 										<a href="/categories/sense_animo_de_lucre" class="NavMenu-service-link">Sense ànim de lucre</a>
@@ -480,21 +393,12 @@ include_once plugin_dir_path(__FILE__) . 'inc/button-menu.php';
 								Esdeveniments
 							</button>
 							<div class="collapse" id="collapsedService9">
-<<<<<<< HEAD
 								<a href="/categories/empresas" class="NavMenu-service-link">Empreses</a>
 								<a href="/categories/bodas" class="NavMenu-service-link">Noces</a>
 								<a href="/categories/despedidas-o-cumpleanos" class="NavMenu-service-link">Comiats / anniversari</a>
 								<a href="/categories/dj-o-musica-en-vivo" class="NavMenu-service-link">DJ / Musica en viu</a>
 								<a href="/categories/promocionales" class="NavMenu-service-link">Promocionals</a>
 								<a href="/categories/sin-animo-de-lucro" class="NavMenu-service-link">Sense ànim de lucre</a>
-=======
-								<a href="/categories/empreses" class="NavMenu-service-link">Empreses</a>
-								<a href="/categories/noces" class="NavMenu-service-link">Noces</a>
-								<a href="/categories/comiats_o_aniversari" class="NavMenu-service-link">Comiats / anniversari</a>
-								<a href="/categories/dj_o_musica_en_viu" class="NavMenu-service-link">DJ / Musica en viu</a>
-								<a href="/categories/promocionals" class="NavMenu-service-link">Promocionals</a>
-								<a href="/categories/sense_animo_de_lucre" class="NavMenu-service-link">Sense ànim de lucre</a>
->>>>>>> f4a7216dd0602431414c33c60f836de7e29fa3ff
 							</div>
 						</div>
 					</div>
