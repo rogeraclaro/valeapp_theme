97ad04864d26500ea11f320f3aa720985e5d0a97
Roger <roger.masellas@inthe.es>
2024-07-12 18:39:24 +0200

    fixed login screens

--- page-iniciar-sessio-clients.php
+++ page-iniciar-sessio-clients.php
@@ -20,7 +20,7 @@ get_header();
     <div class="row justify-content-center align-items-center">
         <div class="col-12">
             <form method="POST" id="" action="/wp-login.php">
-                <h2 class="title">Inicio de sesion Clientes</h2>
+                <h2 class="title">Inici de sessió Client</h2>
                 <input value="" type="text" id="user_login" name="log" class="text-form" placeholder="Email" required>
                 <input value="" type="password" id="user_pass" name="pwd" class="text-form" placeholder="Contraseña"
                     required>
--- page-iniciar-sessio-proveidors.php
+++ page-iniciar-sessio-proveidors.php
@@ -19,7 +19,7 @@ get_header();
     <div class="row justify-content-center align-items-center">
         <div class="col-12" id="login-code-container">
             <form method="POST" id="" action="/wp-login.php">
-                <h2 class="title">Inicio de sesion Proveedor</h2>
+                <h2 class="title">Inici de sessió Proveidor</h2>
                 <input value="" type="text" id="user_login" name="log" class="text-form" placeholder="Email" required>
                 <input value="" type="password" id="user_pass" name="pwd" class="text-form" placeholder="Contraseña"
                     required>
