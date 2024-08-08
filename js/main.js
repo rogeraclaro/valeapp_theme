833b1b8d482f56eb05e4a809a4c88c82e9b77498
Roger <roger.masellas@inthe.es>
2024-08-08 22:29:02 +0200

    fix jquery error

--- js/main.js
+++ js/main.js
@@ -56,15 +56,15 @@ jQuery(document).ready(function () {
   // Menu.init();
 
   // Eye password
-  const togglePassword = document.querySelector("#togglePassword");
-  const password = document.querySelector("#password");
+  // const togglePassword = document.querySelector("#togglePassword");
+  // const password = document.querySelector("#password");
 
-  togglePassword.addEventListener("click", function (e) {
-    // toggle the type attribute
-    const type =
-      password.getAttribute("type") === "password" ? "text" : "password";
-    password.setAttribute("type", type);
-    // toggle the eye slash icon
-    this.classList.toggle("bi-eye-slash");
-  });
+  // togglePassword.addEventListener("click", function (e) {
+  //   // toggle the type attribute
+  //   const type =
+  //     password.getAttribute("type") === "password" ? "text" : "password";
+  //   password.setAttribute("type", type);
+  //   // toggle the eye slash icon
+  //   this.classList.toggle("bi-eye-slash");
+  // });
 });
